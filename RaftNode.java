59d6a9267bd8a245fd54d8a5b6e5d86b2fe55a56
AbigailBian <gbian0321@gmail.com>
2018-03-05 15:13:25 -0500

    Implement ElectionThread and AppendEntries Thread

--- RaftNode.java
+++ RaftNode.java
@@ -1,10 +1,14 @@
 import lib.*;
 
 import java.io.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class RaftNode implements MessageHandling {
     private int id;
@@ -26,7 +30,7 @@ public class RaftNode implements MessageHandling {
     private int[] matchIndices;
 
     // Randomized the timeout value of each node
-    private Random random;
+    private int timeOut;
 
     /* The paper's section 5.2 mentions election timeouts in the range of 150 to 300 milliseconds.
      * Such a range only makes sense if the leader sends heartbeats considerably more often than
@@ -38,13 +42,14 @@ public class RaftNode implements MessageHandling {
     // Interval of sending the heartbeat
     // The tester limits you to 10 heartbeats per second
     private static final int heartbeatInterval = 100;
+    private LinkedBlockingDeque<Boolean> heartBeatQueue;
 
 
     // Count the number of votes;
     private int voteCount;
 
-    // Type
-    private String type;
+    // State
+    private String state;
 
     public RaftNode(int port, int id, int num_peers) {
         this.id = id;
@@ -59,43 +64,160 @@ public class RaftNode implements MessageHandling {
 
         this.nextIndices = new int[num_peers];
         this.matchIndices = new int[num_peers];
-
         Arrays.fill(nextIndices, 1);
 
+        Random random = new Random();
+        this.timeOut = maxTimeoutVal - random.nextInt() % minTimeoutVal;
+        this.heartBeatQueue = new LinkedBlockingDeque<>();
+
         this.voteCount = 0;
-        new Thread () {
-            public void perform() {
-                if ()
+        this.state = "follower";
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                perform();
             }
-        };
+        }).start();
+    }
+
+    public void perform() {
+        if (this.state.equals("follower") || this.state.equals("candidate")) {
+            try {
+                Boolean ans = heartBeatQueue.poll(this.timeOut, TimeUnit.MILLISECONDS);
+                if (ans == null) {
+                    // If timeout, change its state to candidate and starts election.
+                    this.state = "candidate";
+                    this.currentTerm++;
+                    this.voteCount++;
+                    this.votedFor = this.id;
+
+                    for (int i = 0; i < num_peers; i++) {
+                        if (i == this.id) {
+                            continue;
+                        }
+                        RequestVoteArgs requestArgs = new RequestVoteArgs(this.currentTerm,
+                                                                          this.id,
+                                                                          this.entries.get(entries.size() - 1).getIndex(),
+                                                                          this.entries.get(entries.size() - 1).getTerm());
+                        RequestVoteThread requestThread = new RequestVoteThread(this.id, i, requestArgs);
+                        requestThread.run();
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            leaderBroadCast();
+        }
     }
 
     private class AppendEntryThread extends Thread {
+        private AppendEntriesArgs appendArgs;
+        private int destID;
 
-        AppendEntryThread() {
-
+        AppendEntryThread(int destID, AppendEntriesArgs args) {
+            this.appendArgs = args;
+            this.destID = destID;
         }
 
         @Override
         public void run() {
+            Message request = serialize(MessageType.AppendEntriesArgs, id, this.destID, this.appendArgs);
+            Message reply;
 
+            try {
+                reply = lib.sendMessage(request);
+                // failure
+                if (reply == null) {
+                    return;
+                }
+                AppendEntriesReply appendReply = (AppendEntriesReply) deserialize(reply);
+
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private class RequestVoteThread extends Thread {
-
-        RequestVoteThread() {
-
+        private int srcID;
+        private int destID;
+        private RequestVoteArgs requestArgs;
+        RequestVoteThread(int srcID, int destID, RequestVoteArgs args) {
+            this.srcID = srcID;
+            this.destID = destID;
+            this.requestArgs = args;
         }
 
         @Override
         public void run() {
+            // pack up the Message and send those msgs to the peers using TransportLib
+            // then unpack and take actions
+            Message request = serialize(MessageType.RequestVoteArgs, this.srcID, this.destID, requestArgs);
+            Message reply;
+            try {
+                reply = lib.sendMessage(request);
+                // failure
+                if (reply == null) {
+                    return;
+                }
 
+                RequestVoteReply deserializedReply = (RequestVoteReply) deserialize(reply);
+                boolean voteGranted = deserializedReply.voteGranted;
+                int replierTerm = deserializedReply.term;
+
+
+                if (replierTerm < currentTerm) {
+                    return;
+                } else if (replierTerm > currentTerm) { // replier is more up-to-date, step down
+                    state = "follower";
+                    voteCount = 0;
+                    currentTerm = replierTerm;
+                    votedFor = -1;
+                } else {
+                    if (voteGranted) {
+                        voteCount++;
+                        if (voteCount > num_peers / 2) {
+                            state = "leader";
+                            heartBeatQueue.offer(true);
+                            for (int i = 0; i < num_peers; i++) {
+                                nextIndices[i] = entries.get(entries.size() - 1).getIndex() + 1;
+                                matchIndices[i] = 0;
+                            }
+                            heartBeatQueue.offer(true);
+                        }
+                    }
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void leaderBroadCast() {
+        for (int i = 0; i < num_peers; i++) {
+            if (i == id) {
+                continue;
+            }
+
+
+            int prevLogIndex = this.nextIndices[i] - 1;
+            int prevLogTerm = entries.get(prevLogIndex).getTerm();
+
+            List<LogEntry> toBeAppendEntries = entries.subList(prevLogIndex + 1, entries.size());
+            AppendEntriesArgs appendArgs = new AppendEntriesArgs(this.currentTerm, this.id, prevLogIndex, prevLogTerm,
+                                                                 toBeAppendEntries, commitIndex);
+            AppendEntryThread appendThread = new AppendEntryThread(i, appendArgs);
+            appendThread.start();
         }
     }
 
 
-    /************** Serialize and Deserialize ****************/
+    /******************************* Serialize and Deserialize *************************************/
 
     public Message serialize(MessageType type, int src_addr, int dest_addr, Object object) {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -143,6 +265,8 @@ public class RaftNode implements MessageHandling {
 
         return in;
     }
+
+    /***********************************************************************************/
 
     /*
      *call back.
--- lib/LogEntry.java
+++ lib/LogEntry.java
@@ -1,9 +1,33 @@
 package lib;
 
 public class LogEntry {
-    public int index;
-    public int term;
-    public Integer command;
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public Integer getCommand() {
+        return command;
+    }
+
+    public void setCommand(Integer command) {
+        this.command = command;
+    }
+
+    private int index;
+    private int term;
+    private Integer command;
 
     public LogEntry(int index, int term, Integer command) {
         this.index = index;
